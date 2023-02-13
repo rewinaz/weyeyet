1fa8de3d875818f555bf9e9216b3281f97bbd13e
rewina zerou <rewinazerou6@gmail.com>
2023-02-14 00:39:23 +0300

    added some configurations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,7 +9,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bc-s-darkblue": "#282A3A",
+        "bc-m-darkblue": "#1E202D",
+        "bc-darkblue": "#0B0B15",
+        "bc-violet": {
+          500: "#5F7CD9",
+          900: "#5F7CF9",
+        },
+        "bc-danger": "#CA4343",
+        "bc-white": "#FFFFFF",
+      },
+      ringColor: {
+        DEFAULT: "",
+      },
+    },
   },
   plugins: [],
 };
